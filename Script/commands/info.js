9e5ba0f9082c5cdecb5053abaf434532f180c16d
hridayhassanshanto-bit <hridayhassanshanto@gmail.com>
2026-08-17 11:50:52 +0800

    Update info.js

--- Script/commands/info.js
+++ Script/commands/info.js
@@ -20,25 +20,34 @@ module.exports.run = async function({ api, event }) {
  const callback = () => api.sendMessage({
  body: `
 ┏━━━━━━━━━━━━━━━━┓
-┃  🌟𝐎𝐖𝐍𝐄𝐑 𝐈𝐍𝐅𝐎🌟
-┣━━━━━━━━━━━━━━━━┫
-┃👤𝐍𝐀𝐌𝐄      :𝐑𝐀𝐇𝐀𝐓
-┃🚹𝐆𝐄𝐍𝐃𝐄𝐑    :𝐌𝐀𝐋𝐄
-┃🎂𝐀𝐆𝐄       :𝟏𝟔
-┃🕌𝐑𝐄𝐋𝐈𝐆𝐈𝐎𝐍 : 𝐈𝐒𝐋𝐀𝐌
-┃🏫𝐄𝐃𝐔𝐂𝐀𝐓𝐈𝐎𝐍 :বয়ড়া ইসরাইল
-┃🏡𝐀𝐃𝐃𝐑𝐄𝐒𝐒 :জামালপুর,বাংলাদেশ
-┣━━━━━━━━━━━━━━━━┫
-┃𝐓𝐈𝐊𝐓𝐎𝐊 :@where.is.she15
-┃📢𝐓𝐄𝐋𝐄𝐆𝐑𝐀𝐌 :দিবো না🥴🤪
-┃🌐𝐅𝐀𝐂𝐄𝐁𝐎𝐎𝐊 :বায়ো-তে আছে
-┣━━━━━━━━━━━━━━━━┫
+┃
+❤️ ADMIN ❤️
+👤 Name: হৃদয় হাসান শান্ত
+❤️ Nick Name: shonto_
+⚡ Gender: MALE
+🔥 Attitude: 👑 KING
+🎮 Free Fire: LOVER
+🎓 Prasuna: 🗣️ Prrrbbasi 😭
+💼 Work: CONTRACTION
+❤️ Relation: SINGEL
+🎂 Age: 22+ | Birthday: APRIL 24
+🩸 Blood: A+ | Height: 5'6" | Wit: 54+
+🌙 Religion: ISLAM
+🏠 Address: ⊕ Bogura
+📱 Social Media
+🔵 Facebook: https://www.facebook.com/share/1E299MzHcO/
+🟢 WhatsApp: +601116710390
+🟣 Instagram: https://m.me/j/AbbDv4dJZVcKj6b1/
+🎵 TikTok: মালয়েশিয়া সিঙ্গেল বয়
+🔷 Telegram: mrjuweI2025
+📧 Gmail: hridaysanto30@gmail.com
+⭐ Updated Time: 17/04/2026 07:14:19 PM
 ┃🕒𝐔𝐏𝐃𝐀𝐓𝐄𝐃 𝐓𝐈𝐌𝐄 :${time}
 ┗━━━━━━━━━━━━━━━━┛`,
  attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
 
- return request("https://i.imgur.com/FJI61jS.jpeg")
+ return request("https://i.imgur.com/SrRtp8O.jpeg")
  .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
  .on('close', () => callback());
 };
